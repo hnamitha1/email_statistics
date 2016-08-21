f22749481e476d7b9b818e4ff2e6c1c897d74c68
hnamitha1 <namitha.h@gmail.com>
2016-08-21 21:26:08 +0100

    changing the test

--- spec/features/email_stat_spec.rb
+++ spec/features/email_stat_spec.rb
@@ -23,5 +23,28 @@ describe 'display email statistics'  do
       expect(page).to have_content 'Emails click: 1'
     end
 	end
-	
+
+	describe 'open and click rates per email type' do
+    before do
+      3.times { FactoryGirl.create(:email, email_type: 'Order', event: 'send') }
+      2.times { FactoryGirl.create(:email, email_type: 'Order', event: 'open') }
+      1.times { FactoryGirl.create(:email, email_type: 'Order', event: 'click') }
+      5.times { FactoryGirl.create(:email, email_type: 'Shipment', event: 'send') }
+      4.times { FactoryGirl.create(:email, email_type: 'Shipment', event: 'open') }
+      3.times { FactoryGirl.create(:email, email_type: 'Shipment', event: 'click') }
+
+      visit '/'
+    end
+
+    it 'open rate per email type' do
+    	expect(page).to have_content '66.67'
+    	expect(page).to have_content '80.0'
+    end
+
+    it 'click rate per email type' do
+    	expect(page).to have_content '33.33'
+      expect(page).to have_content '60.0'
+    end
+
+	end
 end
