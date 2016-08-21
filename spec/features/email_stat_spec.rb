375f93791bb282b80cddb5628fbd0013d9e68322
Namitha <namitha.h@gmail.com>
2016-08-21 23:38:47 +0100

    Update email_stat_spec.rb

--- spec/features/email_stat_spec.rb
+++ spec/features/email_stat_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe 'display email statistics'  do
-	describe 'email count by event' do
-		before do
-			FactoryGirl.create(:email, event: 'send')
-			FactoryGirl.create(:email, event: 'open')
-			FactoryGirl.create(:email, event: 'open')
-			FactoryGirl.create(:email, event: 'click')
+  describe 'email count by event' do
+    before do
+	FactoryGirl.create(:email, event: 'send')
+	FactoryGirl.create(:email, event: 'open')
+	FactoryGirl.create(:email, event: 'open')
+	FactoryGirl.create(:email, event: 'click')
 
-			visit ('/')
-		end
+	visit ('/')
+    end
 
-		it 'total number of emails sent' do
+    it 'total number of emails sent' do
       expect(page).to have_content 'Emails sent: 1'
     end
 
@@ -22,9 +22,9 @@ describe 'display email statistics'  do
     it 'total number of emails click' do
       expect(page).to have_content 'Emails click: 1'
     end
-	end
+  end
 
-	describe 'open and click rates per email type' do
+  describe 'open and click rates per email type' do
     before do
       3.times { FactoryGirl.create(:email, email_type: 'Order', event: 'send') }
       2.times { FactoryGirl.create(:email, email_type: 'Order', event: 'open') }
@@ -45,6 +45,5 @@ describe 'display email statistics'  do
     	expect(page).to have_content '33.33'
       expect(page).to have_content '60.0'
     end
-
-	end
+  end
 end
